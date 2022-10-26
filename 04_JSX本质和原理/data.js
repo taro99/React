8c8641b9b4a166e67bd9d5e46e49ceedc8835447
kcons <193791789@qq.com>
2022-10-26 14:23:40 +0800

    first commit

---
+++ 04_JSX本质和原理/data.js
@@ -0,0 +1,30 @@
+const book = [
+  {
+    id: 1,
+    name: '狼道',
+    price: 19.7,
+    num: 0,
+    count: 0,
+  },
+  {
+    id: 2,
+    name: '三毛流浪记',
+    price: 29.7,
+    num: 0,
+    count: 0,
+  },
+  {
+    id: 3,
+    name: '鬼谷子',
+    price: 2.7,
+    num: 0,
+    count: 0,
+  },
+  {
+    id: 4,
+    name: '阿里人的答案书',
+    price: 26.99,
+    num: 0,
+    count: 0,
+  },
+]
